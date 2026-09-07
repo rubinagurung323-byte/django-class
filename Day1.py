6966d323319c648d123d0ea2eda8fa11a7e5fe0e
Rubina Gurung <rubinagurung.323@gmail.com>
2026-09-07 15:46:08 +0545

    first day work

---
+++ Day1.py
@@ -0,0 +1,18 @@
+first_name = "Rubina"
+_lastname = 'Gurung'
+
+age = 20
+weight = 47.5
+
+isOkay = True
+x = True
+
+print(first_name)
+print(_lastname)
+
+
+print(type(first_name))
+print(type(age))
+print(type(weight))
+print(type(isOkay))
+print(type(x))
