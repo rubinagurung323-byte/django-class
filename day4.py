62cbeab6fce514af9a30fca1abe956ef11aff0a8
Rubina Gurung <rubinagurung.323@gmail.com>
2026-09-09 16:45:47 +0545

    day4

---
+++ day4.py
@@ -0,0 +1,37 @@
+for letter in "Django":
+    print(letter)
+
+fruits = ["raspberry","strawberry","blueberry"]
+for fruit in fruits:
+    print(fruit)
+
+for i in range(5):
+    print(i)
+
+for j in range(1,10):
+    print(j)
+
+for j in range(1,10,2):
+    print(j)
+
+# 1 - 10 sum ???
+total = 0
+for n in range(1,10):
+
+    total = total + n
+print(total)
+count = 5
+while count > 0:
+    print(count)
+    # count-=1
+    # count = count - 1
+    count = count -1 
+print("finished") 
+
+for n in range(10):
+    if n %2 == 0:
+        print(f"Even:{n}")
+        #print("Even:" + str (n))
+    else:
+        print(f"Odd:{n}" )
+        #print("Odd:" + str (n))
